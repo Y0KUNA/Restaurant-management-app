7ebf2cb58de6d27918d0c29e29e38e9262cdb9b4
Y0KUNA <lequanghuy18062004@gmail.com>
2025-05-20 23:01:12 +0700

    Add code

---
+++ Code/restaurant-management/src/main/java/com/ttcs/restaurant/management/RestaurantManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ttcs.restaurant.management;
+
+/**
+ *
+ * @author Admin
+ */
+public class RestaurantManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
